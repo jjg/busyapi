39edc562a1f7794da632cd97d1af9b38937a3298
James Tankersely <jim.tank@live.com>
2018-05-14 23:21:02 -0500

    Initial changes and test_log.txt, R&D project to improve API process performance

--- routes/api/usages.js
+++ routes/api/usages.js
@@ -1,12 +1,17 @@
 module.exports = function(app){
-    app.post('/api/usages', function(req, res){
+
+    // reference: https://hackernoon.com/6-reasons-why-javascripts-async-await-blows-promises-away-tutorial-c7ec10518dd9
+    app.post('/api/usages', async function(req, res) {
 
         // Store the supplied usage data
         app.usages.push(req.body);
 
         var usageId = app.usages.length;
-        console.log('Stored usage count: ' + usageId);
 
+        // Only log output every 100 posts
+        if ((usageId % 100) === 0) {
+            console.log('Stored usage count: ' + usageId);
+        }
         res.status(201).json({'id':usageId});
     });
 }
